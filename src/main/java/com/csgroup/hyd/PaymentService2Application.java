d35a4c7670492e3c1dc69beafc4c65ef5ed0decc
vineeth <vineethbakthavari123@gmail.com>
2024-08-18 10:38:50 +0530

    payment module

---
+++ src/main/java/com/csgroup/hyd/PaymentService2Application.java
@@ -0,0 +1,13 @@
+package com.csgroup.hyd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentService2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentService2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/csgroup/hyd/PaymentService2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csgroup.hyd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentService2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
